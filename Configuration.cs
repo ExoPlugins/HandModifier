ca5181efd663e3fee15625d63dc63ee1145c68d6
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-04-05 13:02:26 -0300

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+
+namespace HandModifier
+{
+	public class Configuration : IRocketPluginConfiguration
+	{
+		public byte Width;
+		public byte Height;
+
+
+		public void LoadDefaults()
+		{
+			Width = 0;
+			Height = 0;
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,52 @@
+﻿using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using System.Linq;
+
+namespace HandModifier
+{
+    public class MQSPlugin : RocketPlugin<Configuration>
+    {
+		public static MQSPlugin Instance;
+
+		protected override void Load()
+		{
+			MQSPlugin.Instance = this;
+
+			UnturnedPlayerEvents.OnPlayerUpdateGesture += GestureChange;
+
+			Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+			Logger.LogWarning($"[{Name}] has been loaded! ");
+			Logger.LogWarning("Dev: MQS#7816");
+			Logger.LogWarning("Join this Discord for Support: https://discord.gg/Ssbpd9cvgp");
+			Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+		}
+
+		private void GestureChange (UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+		{
+			if (gesture == UnturnedPlayerEvents.PlayerGesture.InventoryOpen)
+			{
+				{
+					if (player.GetPermissions().Any(x => x.Name == "modifier.ignore"))
+                    {
+						return;
+                    }
+					player.Inventory.items[2].resize(Configuration.Instance.Width, Configuration.Instance.Height);
+				}
+			}
+		}
+
+		protected override void Unload()
+		{
+			MQSPlugin.Instance = null;
+
+			UnturnedPlayerEvents.OnPlayerUpdateGesture -= GestureChange;
+
+			Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+			Logger.LogWarning($"[{Name}] has been unloaded! ");
+			Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+		}
+	}
+}
